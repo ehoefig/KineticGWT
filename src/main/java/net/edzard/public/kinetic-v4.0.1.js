e76aea87805447d4c0ad17907c1039b1629dfa6c
neothemachine <maik.riechert@arcor.de>
2013-06-06 15:55:56 +0200

    hotfix for https://github.com/ericdrowell/KineticJS/issues/244

--- src/main/java/net/edzard/public/kinetic-v4.0.1.js
+++ src/main/java/net/edzard/public/kinetic-v4.0.1.js
@@ -3904,7 +3904,7 @@ Kinetic.Shape.prototype = {
             // color fill
             if(Kinetic.Type._isString(fill)) {
                 context.fillStyle = fill;
-                context.fill(context);
+                context.fill();
             }
             // pattern
             else if(fill.image) {
@@ -3917,7 +3917,7 @@ Kinetic.Shape.prototype = {
                 }
 
                 context.fillStyle = context.createPattern(fill.image, repeat);
-                context.fill(context);
+                context.fill();
             }
             // linear gradient
             else if(!s.radius && !e.radius) {
@@ -3929,7 +3929,7 @@ Kinetic.Shape.prototype = {
                     grd.addColorStop(colorStops[n], colorStops[n + 1]);
                 }
                 context.fillStyle = grd;
-                context.fill(context);
+                context.fill();
             }
             // radial gradient
             else if((s.radius || s.radius === 0) && (e.radius || e.radius === 0)) {
@@ -3941,11 +3941,11 @@ Kinetic.Shape.prototype = {
                     grd.addColorStop(colorStops[n], colorStops[n + 1]);
                 }
                 context.fillStyle = grd;
-                context.fill(context);
+                context.fill();
             }
             else {
                 context.fillStyle = 'black';
-                context.fill(context);
+                context.fill();
             }
             context.restore();
         }
